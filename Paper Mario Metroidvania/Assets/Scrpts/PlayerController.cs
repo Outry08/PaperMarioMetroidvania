023b9700cc15cd62735b2983970bbd1309595b29
rmckinno <rmckinno@uoguelph.ca>
2023-06-01 22:43:17 -0400

    Ground collision added, figuring out jumping next

--- Paper Mario Metroidvania/Assets/Scrpts/PlayerController.cs
+++ Paper Mario Metroidvania/Assets/Scrpts/PlayerController.cs
@@ -7,18 +7,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public InputAction playerControls;
+    public InputAction playerMovement;
+    public InputAction playerJump;
     Vector2 moveDirection = Vector2.zero;
     public float moveSpeed = 5f;
+    public float jumpSpeed = 10f;
 
     private void OnEnable()
     {
-        playerControls.Enable();
+        playerMovement.Enable();
+        playerJump.Enable();
     }
 
     private void OnDisable()
     {
-        playerControls.Disable();
+        playerMovement.Disable();
+        playerJump.Disable();
     }
 
     // Start is called before the first frame update
@@ -31,7 +35,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Taking the user's key input
-        moveDirection = playerControls.ReadValue<Vector2>();
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        moveDirection = playerMovement.ReadValue<Vector2>();
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+        //if (playerJump.triggered)
+        //{
+        //    rb.velocity = new Vector2(moveDirection.x * moveSpeed, jumpSpeed);
+        //}
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
     }
 }
