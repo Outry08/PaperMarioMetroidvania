bddb15233f01c51079ac6ca90726b5bcced93e67
rmckinno <rmckinno@uoguelph.ca>
2023-06-01 21:09:29 -0400

    First commit

---
+++ Paper Mario Metroidvania/Assets/Scrpts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public InputAction playerControls;
+    Vector2 moveDirection = Vector2.zero;
+    public float moveSpeed = 5f;
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Taking the user's key input
+        moveDirection = playerControls.ReadValue<Vector2>();
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
