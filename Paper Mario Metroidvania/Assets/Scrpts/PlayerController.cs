097e125505ec18825946596913c65c8b324318e9
rmckinno <rmckinno@uoguelph.ca>
2023-06-03 13:43:24 -0400

    Mario now turns around to face the direction he walks.

--- Paper Mario Metroidvania/Assets/Scrpts/PlayerController.cs
+++ Paper Mario Metroidvania/Assets/Scrpts/PlayerController.cs
@@ -8,43 +8,83 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public InputAction playerMovement;
-    public InputAction playerJump;
+    //public InputAction playerJump;
     Vector2 moveDirection = Vector2.zero;
+    Vector2 facingLeft;
+    Vector2 facingRight;
     public float moveSpeed = 5f;
     public float jumpSpeed = 10f;
+
+    bool onGround = false;
+    bool isFacingLeft = false;
 
     private void OnEnable()
     {
         playerMovement.Enable();
-        playerJump.Enable();
+        //playerJump.Enable();
     }
 
     private void OnDisable()
     {
         playerMovement.Disable();
-        playerJump.Disable();
+        //playerJump.Disable();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        facingLeft = new Vector2(-transform.localScale.x, transform.localScale.y);
+        facingRight = new Vector2(transform.localScale.x, transform.localScale.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Taking the user's key input
+        //Moving left and right
         moveDirection = playerMovement.ReadValue<Vector2>();
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
-        //if (playerJump.triggered)
-        //{
-        //    rb.velocity = new Vector2(moveDirection.x * moveSpeed, jumpSpeed);
-        //}
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isFacingLeft = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            isFacingLeft = false;
+        }
+
+        if (isFacingLeft)
+        {
+            transform.localScale = facingLeft;
+        }
+        if (!isFacingLeft)
+        {
+            transform.localScale = facingRight;
+        }
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, jumpSpeed);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, 1);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.name == "Ground")
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            onGround = false;
+        }
     }
 }
